e5f5e17424117fe584b6798a7fb52fbb18a3352b
andreasbloomquist <andreasbloomquist@gmail.com>
2015-06-15 09:27:12 -0700

    Add FFaker

--- full_homework/db/seeds.rb
+++ full_homework/db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
+
+(1..10).each do |x|
+	user_params = {}
+	user_params[:email] = "#{x}#{FFaker::Internet.email}"
+	user_params[:password] = "testtest"
+	user_params[:password_confirmation] = user_params[:password]
+	user_params[:first_name] = FFaker::Name.first_name
+	user_params[:last_name] = FFaker::Name.last_name
+	new_user = User.create(user_params)
+
+	(1..10).each do
+
+		new_article = Article.new
+		new_article.title = FFaker::HipsterIpsum.words(rand(8)+2).join(" ")
+		new_article.content = FFaker::HipsterIpsum.paragraphs(1+ rand(4)).join("\n")
+		new_article.save
+		new_user.articles.push new_article
+	end
+end
